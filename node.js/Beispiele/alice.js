f8c97364ec94fb991ba599250b774abc023daef7
Sven Lilienthal <sven.piehl@codeacid.com>
2025-02-11 14:15:58 +0100

    alice analyse solutions

--- node.js/Beispiele/alice.js
+++ node.js/Beispiele/alice.js
@@ -1,19 +1,44 @@
 const fs = require("fs")
 
 
-// function readfilecontent( err, content ) {
-// }
-// fs.readFile( "alice.txt", readfilecontent) 
-
-
-
 fs.readFile( "alice.txt", "utf-8",( err, content ) => {
 	
 	if(err) {
 		console.error("Achtung, Datei konnte nicht geöffnet werden")
 	}
 	else {
-		console.log(content.replace(/[^a]/g, "").length)
+		console.log( "lines: " , content.split("\n").length ) 
+		console.log( "zeichen: " , content.length ) 
+		console.log(content.replace(/[^A]/g, ""))
+		console.log("Zeichen A: ",content.replace(/[^A]/g, "").length)
+		console.log("Zeichen a: ",content.replace(/[^a]/g, "").length)
+		
+		const chars = {}
+		
+		content.split("").forEach( chr => {
+			if( Object.keys(  chars ).includes( chr ) ) {
+				chars[ chr ]++;
+			}
+			else {
+				chars[ chr ]  = 1
+			}
+		})
+		
+		const sorted = Object.entries( chars ). sort( (a,b) => b[1] - a[1] )
+		
+		
+		console.log( sorted.slice(0,5).map( value => {
+			value.push( value[1] / content.length * 100 )
+			return value
+		}))
+		
+		const total = sorted.slice(0,5).map(v=>v[1]).reduce( (a,b)  => a + b)
+		console.log( total)		
+		console.log( sorted.slice(0,5).map( value => {
+			value.push( value[1] / total * 100 )
+			return value
+		}))
+		
 	}
 	
 })
