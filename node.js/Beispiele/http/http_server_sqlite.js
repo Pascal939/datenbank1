4c2de9cde245965453267b9b557e4c4a3ab9e330
Sven Lilienthal <sven.piehl@codeacid.com>
2025-02-14 09:19:10 +0100

    http-server with sqlite -- added comments

--- node.js/Beispiele/http/http_server_sqlite.js
+++ node.js/Beispiele/http/http_server_sqlite.js
@@ -2,44 +2,109 @@ const http = require("http")
 const sqlite3 = require("sqlite3")
 
 
-
+//
+// Eine Funktion zur Abfrage aller Produkte aus der Datenbank
+//
 function getAllProducts() {
-	return new Promise( ( resolve ) => {
+
+	//
+	// Erzeuge ein neues Promise
+	//
+	return new Promise( ( resolve ) => {	
 		
+		//
+		// Öffne die Datenbank-Datei "database.db"
+		//
 		const db = new sqlite3.Database('database.db')
-		
+	
+		//
+		// Abfrage der Datenbank-Produkte, erhalte das Ergebnis als Array [rows]
+		//
 		db.all( `SELECT * FROM product`,  (err,rows) => {
+			
+			//
+			// Erfülle das Promise
+			//			
 			resolve( rows) 
 		})
-
+		
+		//
+		// Beende die Datenbankverbindung
+		//	
 		db.close()
 	})
 }
 
+
+//
+// Erstelle den HTTP-Server
+//	
 const server = http.createServer(  (req,res) => {
 	
-
+	//
+	// Wurde kein besonderer Pfad beim Request angefragt
+	//
 	if( req.url == "/" ) {	
+	
+		//
+		// rufe die Funktion getAllProducts auf
+		//
 		getAllProducts()
+		
+		//
+		// getAllProducts Promise erfüllt
+		//		
 		.then( rows => {
+			
+			
+			//
+			// Sende Statuscode 200 an den Benutzer
+			//
 			res.writeHead( 200 ) 
+			
+			//
+			// HTML .....
+			//
 			let html = ""
 			let col = 0;
 			let bgcolors = ['#888888', '#fff']
 			
+			
+			//
+			// Für jedes Ergebnis der Datenbankabfrage...
+			//
 			rows.forEach( row => {
+				
+				//
+				// Füge den ganzen Kram zu einem HTML-String zusammen
+				//
 				html += `<div style='float:left; background-color:${bgcolors[col]}'>${row.name}</div><div style='${bgcolors[col]}'>${row.catId}</div>`
 				col++;
 				if(col > 1) col = 0
 			} )
 			
-			
+			//
+			// HTML.....
+			//			
 			html = `<html><body>${html}</body></html>`
 			
+			
+			//
+			// Sende den HTML-String zum Client
+			//	
 			res.end( html );
 		})
 	}
+	
+	//
+	// Request URL beinhaltet einen Dateinamen (z.B. favicon.ico) 
+	//	
 	else {
+		
+				
+		//
+		// 404 Not Found Error
+		//	
 		res.writeHead( 404 ) 
 		res.end( "" );		
 	}
@@ -47,6 +112,10 @@ const server = http.createServer(  (req,res) => {
 
 })
 
+
+//
+// Starte den Server, horche auf Port 8000 am Interface 127.0.0.1
+//
 server.listen( 8000, "127.0.0.1", (err)=> {
 	if(err) 
 		throw err
